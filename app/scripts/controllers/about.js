5e39df8965eb6d90ae2f6c1159a8857c99258a59
chrisboon <cboon27@gmail.com>
2015-11-17 17:25:08 -0500

    Environment setup
    Followed Yeoman tutorial to setup environment. This involves building some basic functionality for a todo app. For ease of completion I have followed the steps here. This will be wiped out on the next commit when I begin building the TRC UI.
    
    Signed-off-by: chrisboon <cboon27@gmail.com>

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trcApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the trcApp
+ */
+angular.module('trcApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
