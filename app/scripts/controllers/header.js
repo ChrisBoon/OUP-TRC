dcb3a87c494a56a98ca14dd8b9f250c0436dea76
chrisboon <cboon27@gmail.com>
2015-11-18 18:39:28 -0500

    Begin developing product
    I will probably merge my fist few commits until I have a bit of stuff in place to start from...
    
    Signed-off-by: chrisboon <cboon27@gmail.com>

---
+++ app/scripts/controllers/header.js
@@ -0,0 +1,37 @@
+'use strict';
+
+//controller for Header element
+
+angular.module('trcApp')
+  .controller('HeaderCtrl', ['$scope', '$location', function($scope, $location) {
+
+  	$scope.$location = $location;
+
+  	//sets class on branding element to adjust styling depnding on current view
+  	//probably want to move this to a servce or directive or something
+  	$scope.locationClass = function (){
+  		var locationString = $location.path();
+  		if (locationString === '/'){
+  			return 'home';
+  		}
+  		else{
+  			return 'page';
+  		}
+  	};
+
+  	//defines content of dropdown on hamburger
+    $scope.otherStuff = [
+      {
+      	'text': 'Help',
+      	'link': '/help'
+      },
+      {
+      	'text': 'Settings',
+      	'link': '/settings'
+      },
+      {
+      	'text': 'Sign out',
+      	'link': '/sign-out'
+      }
+    ];
+  }]);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,16 +1,61 @@
 'use strict';
 
 angular.module('trcApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.todos = [];
+  .controller('MainCtrl', function ($scope, localStorageService) {
 
-    $scope.addTodo = function () {
-	  $scope.todos.push($scope.todo);
-	  $scope.todo = '';
-	};
+  	$scope.showWelcome = true;
 
-	$scope.removeTodo = function (index) {
-	  $scope.todos.splice(index, 1);
-	};
+
+	$scope.getWelcomeStatus = function(){
+      if (localStorageService.get('isWelcomeHidden') === true) {
+        $scope.showWelcome = false;
+      }
+    };
+
+    //Updates localstorage so we can use getLang next time a user is here.
+    $scope.setWelcomeStatus = function(){
+    	$scope.showWelcome = false;
+	    localStorageService.set('isWelcomeHidden',true);
+    };
+
+    $scope.browseMethods = [
+      {
+      	'title': 'Browse by Resource Type',
+      	'class':'browse-type-1',
+      	'para': 'Looking for a particular resource such as Lesson Plans, or Classroom Activities? Choose this option.',
+      	'imgSrc': 'images/browseby-1.jpg',
+      	'link': '/resource'
+      },
+      {
+      	'title': 'Browse by Topic',
+      	'class':'browse-type-2',
+      	'para': 'Want everything for a particular topic? Choose this option.',
+      	'imgSrc': 'images/browseby-2.jpg',
+      	'link': '/topic'
+      },
+      {
+      	'title': 'Custom search',
+      	'class':'browse-type-3',
+      	'para': 'Looking for something specific? Choose this option for advanced filtering options.',
+      	'imgSrc': 'images/browseby-3.jpg',
+      	'link': '/sign-out'
+      }
+    ];
+     $scope.updatesList = [
+      {
+      	'title': 'Newsela Articles',
+      	'class':'new',
+      	'para': 'We’ve added these new things - check them out here!',
+      	'date': 'Nov 2016',
+      	'link': '/resource-files/newsela'
+      },
+      {
+      	'title': 'Classroom Activities',
+      	'class':'updated',
+      	'para': 'We’ve updated all of this resource to include some new stuff that makes them super relavant to users in 2017. Click here to take another look!',
+      	'date': 'Sept 2016',
+      	'link': '/resource-files/classroom-activities'
+      }
+    ];    
 
   });
