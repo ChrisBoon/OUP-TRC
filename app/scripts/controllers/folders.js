128a343ff7cefc01f54e75f51bb17e8216328386
chrisboon <cboon27@gmail.com>
2015-11-20 16:12:37 -0500

    Began setting up Folder view
    Added one JSON file containing mock content.
    Hooked up the 'resource view' page to route to a folder view.
    Added code to pull correct JSON file.
    Tested it worked by printing some of the son to the page.
    
    Signed-off-by: chrisboon <cboon27@gmail.com>

--- app/scripts/controllers/folders.js
+++ app/scripts/controllers/folders.js
@@ -1,17 +1,25 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name trcApp.controller:FoldersCtrl
- * @description
- * # FoldersCtrl
- * Controller of the trcApp
- */
 angular.module('trcApp')
-  .controller('FoldersCtrl', function () {
+  .controller('FoldersCtrl', function ($scope, $http, $routeParams) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    this.Id = $routeParams.id;
+
+    //store variable ref to this for easy reference
+    var thisPage = this;
+    // add hook for json
+    thisPage.json = {};
+
+    //for generating the main links on the homepage
+    //this will be unlikely to change during lifecycle of a product but will vary by product to product so will either be best as separate file or at least separate in dev and bundled in at build phase
+    $http.get('json/'+this.Id+'.json').success(function(data) {
+    	thisPage.json = data;
+    }).error(function(){
+    	console.log('couldnt find the json for this page');
+	});
+
   });
