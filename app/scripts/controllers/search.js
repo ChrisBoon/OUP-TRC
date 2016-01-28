91c3a2765b7dfe3df4078b3568fa297ca1f5dbb1
chrisboon <cboon27@gmail.com>
2016-01-28 13:36:58 -0500

    create view and controller for 'custom search'
    Signed-off-by: chrisboon <cboon27@gmail.com>

---
+++ app/scripts/controllers/search.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trcApp.controller:SearchCtrl
+ * @description
+ * # SearchCtrl
+ * Controller of the trcApp
+ */
+angular.module('trcApp')
+  .controller('SearchCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
