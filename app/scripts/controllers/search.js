ef982b5bf546940fe9224b0a926362fa6b4573b7
chrisboon <cboon27@gmail.com>
2016-01-28 15:31:50 -0500

    search page pulls JSON and can use 'read more' functionality.
    Signed-off-by: chrisboon <cboon27@gmail.com>

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -8,10 +8,45 @@
  * Controller of the trcApp
  */
 angular.module('trcApp')
-  .controller('SearchCtrl', function () {
+  .controller('SearchCtrl', function ($scope, $http, $routeParams, getJSON, $uibModal) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    //store variable ref to this for easy reference
+    var thisPage = this;
+    // add hook for json
+    thisPage.json = {};
+
+	getJSON.allJSON()
+    .then(function(d) {
+      thisPage.json = d;
+    });
+
+
+
+  thisPage.readMore = function () {
+
+    var modalInstance = $uibModal.open({
+      animation: true,
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      controllerAs: 'myModal',
+      resolve: {
+        longText: function () {
+          return thisPage.json.descriptionLong;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    });
+  };
+
+
+
+
   });
